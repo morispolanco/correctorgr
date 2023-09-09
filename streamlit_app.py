05f5f9368de7f5246456d4b2f7057148960e9e9c
morispolanco <morispolanco@gmail.com>
2023-09-08 19:51:50 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import openai
+
+st.title('Corrector gramatical y de puntuación')
+
+openai.api_key = st.sidebar.text_input('Ingresa tu API Key de OpenAI', type='password')
+
+texto = st.text_area('Ingresa el texto a corregir')
+
+if st.button('Corregir texto'):
+    
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=f'Corrige los errores gramaticales y de puntuación en este texto: {texto}',
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    correccion = response.choices[0].text.strip()
+    
+    st.text_area('Texto corregido', value=correccion, height=200)
